0c62f45ba1a1a1ba909697e3580bb2e91350b4a1
Emad Anwer <emadanwer.official@gmail.com>
2023-05-15 01:22:28 +0200

    list of paths - TODO chack_access()

--- test/shell.h
+++ test/shell.h
@@ -7,7 +7,19 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
-
+/**
+ * struct list_path - singly linked list
+ * @path: string
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_path
+{
+	char *path;
+	unsigned int len;
+	struct list_path *next;
+} list_path;
 
 #endif 
