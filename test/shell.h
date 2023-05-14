b3c47f2a3ab2c5afa7b2f79043a136fc08150b91
Kholoudxs55kh <kfattem@gmail.com>
2023-05-14 03:54:35 +0300

    Command Runs Only with Path

---
+++ test/shell.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+
+#endif 
